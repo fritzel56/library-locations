c72fc97963583b442500a2ab0de4af0055685229
fritzel56 <20907117+fritzel56@users.noreply.github.com>
2022-02-06 23:07:22 -0500

    aligning naming. adding ssl context to avoid error.

--- ottawa_libraries.py
+++ ottawa_libraries.py
@@ -2,6 +2,7 @@
 """
 import urllib
 import xmltodict
+import ssl
 
 def get_ottawa_locations():
     """The ottawa library system provides a well structure data set with
@@ -11,9 +12,11 @@ def get_ottawa_locations():
     Returns:
         dict: dictionary of Ottawa library names and their lat/long
     """
+    context = ssl._create_unverified_context()
     ottawa_url = 'https://biblioottawalibrary.ca/branches.xml'
-    ottawa_data = urllib.request.urlopen(url).read()
-    ottawa_libs = {}
-    for library in xml_parsed['Branches']['BranchInfo']:
-        ottawa_libs[library['Name']] = library['Coordinates']
-    return ottawa_libs
+    ottawa_data = urllib.request.urlopen(ottawa_url, context=context).read()
+    libraries = xmltodict.parse(ottawa_data)
+    ottawa_libraries = {}
+    for library in libraries['Branches']['BranchInfo']:
+        ottawa_libraries[library['Name']] = library['Coordinates']
+    return ottawa_libraries
